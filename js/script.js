62c211ad6bbde221e76d2d982f8d607f0a04f34b
Nikita <verstka.avacode@inbox.ru>
2020-10-11 18:20:41 +0300

    work_with_page

--- js/script.js
+++ js/script.js
@@ -24,3 +24,28 @@ const movieDB = {
     ]
 };
 
+
+
+const rightBlock = document.querySelector(".promo__adv");
+rightBlock.remove();
+
+const genre = document.querySelector(".promo__genre");
+genre.textContent = "КОМЕДИЯ";
+
+const bg = document.querySelector(".promo__bg");
+console.log(bg);
+bg.style.background= 'url(../img/bg.jpg)';
+
+movieDB.movies.sort();
+
+const movieList = document.createElement('ul');
+movieList.classList.add('promo__interactive-list');
+
+for (let i = 0; i < movieDB.movies.length; i++ ){
+	const movieItem = document.createElement('li');
+	movieItem.classList.add('promo__interactive-item');
+	movieItem.textContent = `${i+1} - ${movieDB.movies[i]}`;
+	movieList.append(movieItem);
+}
+
+document.querySelector('.promo__interactive-list').replaceWith(movieList);
